e74ef08224bbac8dace6613cd29579490116b32b
Richeir <315310840@qq.com>
2020-05-25 23:55:44 +0800

    初始化代码目录和开发分支

---
+++ src/leetcode/P100/1.two-sum.go
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=1 lang=golang
+ *
+ * [1] Two Sum
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (43.70%)
+ * Likes:    10551
+ * Dislikes: 348
+ * Total Accepted:    1.7M
+ * Total Submissions: 4M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ *
+ * Example:
+ *
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ *
+ *
+ *
+ *
+ */
+package p100
+
+// @lc code=start
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i; j < len(nums); j++ {
+			if i == j {
+				continue
+			}
+
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+// @lc code=end
---
+++ src/leetcode/P100/tests/leetcode_test.go
@@ -0,0 +1,40 @@
+package tests_test
+
+import (
+	"testing"
+)
+
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i; j < len(nums); j++ {
+			if i == j {
+				continue
+			}
+
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+func Test_1(t *testing.T) {
+	a := []int{2, 7, 11, 15}
+	t.Log(a)
+	var result = twoSum(a, 9)
+	if result[0] == 0 && result[1] == 1 {
+		t.Log("OK")
+	} else {
+		t.Log("No")
+	}
+}
+
+func Test_2(t *testing.T) {
+	num := 10
+	if num%2 == 0 { //checks if number is even
+		t.Log("the number is even")
+	} else {
+		t.Log("the number is odd")
+	}
+}
