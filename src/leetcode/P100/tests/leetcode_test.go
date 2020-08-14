b00e5ff90b5a2778edaaa5f749cd55a391d3ee8e
Richeir <dongyiluo@easyreatilpro.com>
2020-08-14 18:51:36 +0800

    每日一题 20200814

--- src/leetcode/P100/tests/leetcode_test.go
+++ src/leetcode/P100/tests/leetcode_test.go
@@ -1,6 +1,7 @@
 package leetcode
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -37,4 +38,52 @@ func Test_2(t *testing.T) {
 	} else {
 		t.Log("the number is odd")
 	}
+}
+
+func Test20200814(t *testing.T) {
+	str := "]"
+	intStr := []rune(str)
+	fmt.Printf("%v \n", intStr)
+	t.Log(len(intStr))
+	targetStack := []rune{}
+	for i := 0; i < len(intStr); i++ {
+		lengTarget := len(targetStack) - 1
+		if lengTarget < 0 {
+			lengTarget = 0
+		}
+		switch intStr[i] {
+		case 40:
+			targetStack = append(targetStack, intStr[i])
+		case 41:
+			if targetStack[lengTarget] != 40 {
+				t.Log("false")
+				return
+			} else {
+				targetStack = append(targetStack[:lengTarget])
+			}
+
+		case 91:
+			targetStack = append(targetStack, intStr[i])
+		case 93:
+			if targetStack[lengTarget] != 91 {
+				t.Log("false")
+				return
+			} else {
+				targetStack = append(targetStack[:lengTarget])
+			}
+
+		case 123:
+			targetStack = append(targetStack, intStr[i])
+		case 125:
+			if targetStack[lengTarget] != 123 {
+				t.Log("false")
+				return
+			} else {
+				targetStack = append(targetStack[:lengTarget])
+			}
+
+		}
+
+	}
+	fmt.Printf("%v \n", targetStack)
 }
